ee204d29d31c02a34bf73894bff7ae11e04dfbce
thijsguelen22 <thijsguelen22@gmail.com>
2015-12-09 14:52:01 +0100

    First working release

---
+++ Convention/js/load.js
@@ -0,0 +1,119 @@
+﻿function loadOne() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            document.getElementById("one").innerHTML = xhttp.responseText;
+        }
+    };
+    xhttp.open("GET", "http://thijs-jan.aoweb.nl/school/AppDatabaseIndex/one.php", true);
+    xhttp.send();
+}
+function loadTwo() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            document.getElementById("two").innerHTML = xhttp.responseText;
+        }
+    };
+    xhttp.open("GET", "http://thijs-jan.aoweb.nl/school/AppDatabaseIndex/two.php", true);
+    xhttp.send();
+}
+function loadThree() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            document.getElementById("three").innerHTML = xhttp.responseText;
+        }
+    };
+    xhttp.open("GET", "http://thijs-jan.aoweb.nl/school/AppDatabaseIndex/three.php", true);
+    xhttp.send();
+}
+function loadFour() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            document.getElementById("four").innerHTML = xhttp.responseText;
+        }
+    };
+    xhttp.open("GET", "http://thijs-jan.aoweb.nl/school/AppDatabaseIndex/four.php", true);
+    xhttp.send();
+}
+function loadFive() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            document.getElementById("five").innerHTML = xhttp.responseText;
+        }
+    };
+    xhttp.open("GET", "http://thijs-jan.aoweb.nl/school/AppDatabaseIndex/five.php", true);
+    xhttp.send();
+}
+function loadSix() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            document.getElementById("six").innerHTML = xhttp.responseText;
+        }
+    };
+    xhttp.open("GET", "http://thijs-jan.aoweb.nl/school/AppDatabaseIndex/six.php", true);
+    xhttp.send();
+}
+function loadSeven() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            document.getElementById("seven").innerHTML = xhttp.responseText;
+        }
+    };
+    xhttp.open("GET", "http://thijs-jan.aoweb.nl/school/AppDatabaseIndex/seven.php", true);
+    xhttp.send();
+}
+function loadEight() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            document.getElementById("eight").innerHTML = xhttp.responseText;
+        }
+    };
+    xhttp.open("GET", "http://thijs-jan.aoweb.nl/school/AppDatabaseIndex/eight.php", true);
+    xhttp.send();
+}
+function loadNine() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            document.getElementById("nine").innerHTML = xhttp.responseText;
+        }
+    };
+    xhttp.open("GET", "http://thijs-jan.aoweb.nl/school/AppDatabaseIndex/nine.php", true);
+    xhttp.send();
+}
+function loadTen() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            document.getElementById("ten").innerHTML = xhttp.responseText;
+        }
+    };
+    xhttp.open("GET", "http://thijs-jan.aoweb.nl/school/AppDatabaseIndex/ten.php", true);
+    xhttp.send();
+} function loadEleven() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            document.getElementById("eleven").innerHTML = xhttp.responseText;
+        }
+    };
+    xhttp.open("GET", "http://thijs-jan.aoweb.nl/school/AppDatabaseIndex/eleven.php", true);
+    xhttp.send();
+}
+function loadTwelve() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            document.getElementById("twelve").innerHTML = xhttp.responseText;
+        }
+    };
+    xhttp.open("GET", "http://thijs-jan.aoweb.nl/school/AppDatabaseIndex/twelve.php", true);
+    xhttp.send();
+}
